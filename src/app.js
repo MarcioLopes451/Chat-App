e5ca8e36cb72c9ec7772524fea77c8857859871e
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2023-12-06 08:36:26 +0000

    add:structure

--- src/app.js
+++ src/app.js
@@ -11,6 +11,11 @@ function add() {
   const inputValue = input.value.trim();
 
   if (inputValue !== "") {
+    const content = document.createElement('div');
+    content.innerHTML = `
+    <p>You</p>
+    <p class="bg-[#93C2ED] rounded-lg text-white">${inputValue}</p>`;
+    document.getElementById('your-text').appendChild(content);
     const url = `https://api.api-ninjas.com/v1/loremipsum?paragraphs=?&max_length=${maxLength}`;
     fetch(url, options)
       .then((res) => res.json())
@@ -23,7 +28,9 @@ function add() {
 function apiData(text) {
   const content = document.createElement('div');
   content.innerHTML = `
-  <p class="bg-green-400 p-2">${text}</p>`;
+  <p>Victor</p>
+  <p class="bg-[#85BB71] rounded-lg text-white">
+  ${text}
+  </p>`;
   document.getElementById("text").appendChild(content);
-  
 }
