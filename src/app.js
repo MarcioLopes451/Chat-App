e19c8557dbb165dd952db2a8de3f26678ee3651c
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2023-12-07 10:10:31 +0000

    add:structure

--- src/app.js
+++ src/app.js
@@ -9,15 +9,21 @@ let options = {
 function add() {
   const input = document.getElementById("placeholder");
   const inputValue = input.value.trim();
+  
 
   if (inputValue !== "") {
     const content = document.createElement('div');
+    const timestamp = new Date()
     content.innerHTML = `
-    <div class="float-right">
+    <div class="float-right w-[200px] mt-5">
+    <div class="flex justify-between items-center">
+    <p class="text-[#696C75] text-xs">${timeAndDate(timestamp)}</p>
     <p>You</p>
-    <p class="bg-[#93C2ED] rounded-lg text-white">${inputValue}</p>
+    </div>
+    <p class="bg-[#93C2ED] rounded-lg text-white p-3">${inputValue}</p>
     </div>`;
     document.getElementById('text').appendChild(content);
+    scrollToBottom()
     const url = `https://api.api-ninjas.com/v1/loremipsum?paragraphs=?&max_length=${maxLength}`;
     fetch(url, options)
       .then((res) => res.json())
@@ -27,14 +33,29 @@ function add() {
   }
 }
 
+function timeAndDate(timestamp){
+const options = {hour : 'numeric', minute: 'numeric', weekday:'long'};
+return new Date(timestamp).toLocaleString('en-GB', options)
+}
+
+function scrollToBottom(){
+    const chat = document.getElementById('chat');
+    chat.scrollTop = chat.scrollHeight
+}
+
 function apiData(text) {
   const content = document.createElement('div');
+  const timestamp = new Date()
   content.innerHTML = `
-  <div class="float-left">
+  <div class="float-left mt-5">
+  <div class="flex justify-between items-center">
   <p>Victor</p>
-  <p class="bg-[#85BB71] rounded-lg text-white">
+  <p class="text-[#696C75] text-xs">${timeAndDate(timestamp)}</p>
+  </div>
+  <p class="bg-[#85BB71] rounded-lg text-white p-3">
   ${text}
   </p>
   </div>`;
   document.getElementById("text").appendChild(content);
+  scrollToBottom()
 }
